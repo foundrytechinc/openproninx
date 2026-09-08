d24cb9230c862aed5b4ff52bc5db73d9591c9808
Вячесалв Пронин <katzer.sla@gmail.com>
2026-09-08 09:17:33 +0300

    Update product version to 0.1.6-dev

--- inc/product.h
+++ inc/product.h
@@ -2,7 +2,7 @@
 #define FNU_PRONINX_PRODUCT_H
 
 #define FNU_PRODUCT_NAME "FNU/OpenProninx"
-#define FNU_PRODUCT_VERSION "Core 0.1.5-dev"
+#define FNU_PRODUCT_VERSION "Core 0.1.6-dev"
 #define FNU_PRODUCT_VENDOR "Foundry Tech Inc."
 #define FNU_PRODUCT_EXPANSION "Foundry is Not Unix"
 #define FNU_VERSION "FNU 1.1"
